e51712364193a018bd2429461c68d0aa63bc1ee1
Julio Cesar Ribeiro <juliooriebir@gmail.com>
2020-09-22 21:43:14 -0400

    Initial Commit

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Image} from 'react-native';
+
+import {Container} from './styles';
+import logoImg from '../../assets/logo.png';
+
+const SignIn: React.FC = () => (
+  <Container>
+    <Image source={logoImg} />
+  </Container>
+);
+
+export default SignIn;
